f22326a02a5c5ce42efb2ad9a9f1ddd37bd4b61f
Damian <damian942@vp.pl>
2018-05-06 18:34:15 +0200

    Dodanie poprawnie działającego gradientu w androidzie

---
+++ Inzynierka/Inzynierka/Inzynierka/CustomRenderer/GradientColorStackMode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inzynierka.CustomRenderer
+{
+    public enum GradientColorStackMode
+    {
+        ToRight,
+        ToLeft,
+        ToTop,
+        ToBottom,
+        ToTopLeft,
+        ToTopRight,
+        ToBottomLeft,
+        ToBottomRight
+    }
+}
---
+++ Inzynierka/Inzynierka/Inzynierka/CustomRenderer/GradientLayout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Inzynierka.CustomRenderer
+{
+    public class GradientLayout : Grid
+    {
+        public string ColorsList { get; set; }
+
+        public Color[] Colors
+        {
+            get
+            {
+                string[] hex = ColorsList.Split(',');
+                Color[] colors = new Color[hex.Length];
+
+                for (int i = 0; i < hex.Length; i++)
+                {
+                    colors[i] = Color.FromHex(hex[i].Trim());
+                }
+
+                return colors;
+            }
+        }
+
+        public GradientColorStackMode Mode { get; set; }
+    }
+}
