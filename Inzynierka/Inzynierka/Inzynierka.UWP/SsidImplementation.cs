a77c4872ed026bf86a4a74f1bccc81a1b40f28c0
Damian <damian942@vp.pl>
2018-05-07 21:05:48 +0200

    Implementacja gradientu na UWP, oraz dodanie ikon Wstępny projekt okien rejestracji odbić

---
+++ Inzynierka/Inzynierka/Inzynierka.UWP/SsidImplementation.cs
@@ -0,0 +1,34 @@
+﻿using Inzynierka.Interfaces;
+using Inzynierka.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SsidImplementation))]
+namespace Inzynierka.UWP
+{
+    class SsidImplementation : ISsid
+    {
+        public string ssidName
+        {
+            get
+            {
+                if (CrossConnectivity.Current.IsConnected)
+                {
+
+                    var profile = Windows.Networking.Connectivity.NetworkInformation.GetInternetConnectionProfile();
+                    var x = profile.GetSignalBars();
+                    if (profile.IsWlanConnectionProfile)
+                    {
+                        return profile.WlanConnectionProfileDetails.GetConnectedSsid() + "  " + (x * (int)25) + "%";
+                    }
+                }
+                return "Brak sieci";
+            }
+        }
+    }
+}
+
